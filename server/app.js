5bb87ba056b1719590630daaa8f8d10aa358a59f
LucasMRC <lucas.reynacordoba22@gmail.com>
2019-05-06 18:24:56 -0300

    refactor application into client/server apps

--- server/app.js
+++ server/app.js
@@ -2,15 +2,18 @@ var express = require("express"),
 app     = express(),
 mongoose = require("mongoose"),
 bodyParser = require("body-parser"),
-expressSanitizer = require("express-sanitizer"),
-methodOverride = require('method-override');
+expressSanitizer = require("express-sanitizer");
 
 mongoose.connect("mongodb://localhost/todo_app");
-app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSanitizer());
-app.set("view engine", "ejs");
-app.use(methodOverride('_method'));
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();  
+});
 
 var todoSchema = new mongoose.Schema({
   text: String,
@@ -18,69 +21,46 @@ var todoSchema = new mongoose.Schema({
 
 var Todo = mongoose.model("Todo", todoSchema);
 
-app.get("/", function(req, res){
-  res.redirect("/todos");
-});
-
 app.get("/todos", function(req, res){
   Todo.find({}, function(err, todos){
     if(err){
       console.log(err);
     } else {
-      if(req.xhr) {
-        res.json(todos);
-      } else {
-        res.render("index", {todos: todos}); 
-      }
+      res.json(todos);      
     }
   })
 });
 
-app.get("/todos/new", function(req, res){
- res.render("new"); 
-});
-
 app.post("/todos", function(req, res){
- req.body.todo.text = req.sanitize(req.body.todo.text);
- var formData = req.body.todo;
- Todo.create(formData, function(err, newTodo){
+  req.body.todo.text = req.sanitize(req.body.todo.text);
+  var formData = req.body.todo;
+  Todo.create(formData, function(err, newTodo){
     if(err){
-      res.render("new");
+      console.log(err)
     } else {
       res.json(newTodo);
     }
-  });
-});
-
-app.get("/todos/:id/edit", function(req, res){
- Todo.findById(req.params.id, function(err, todo){
-   if(err){
-     console.log(err);
-     res.redirect("/")
-   } else {
-      res.render("edit", {todo: todo});
-   }
- });
+  })
 });
 
 app.put("/todos/:id", function(req, res){
- Todo.findByIdAndUpdate(req.params.id, req.body.todo, {new: true}, function(err, todo){
-   if(err){
-     console.log(err);
-   } else {
+  Todo.findByIdAndUpdate(req.params.id, req.body.todo, {new: true}, function(err, todo){
+    if(err){
+      console.log(err);
+    } else {
       res.json(todo);
-   }
- });
+    }
+  })
 });
 
 app.delete("/todos/:id", function(req, res){
- Todo.findByIdAndRemove(req.params.id, function(err, todo){
-   if(err){
-     console.log(err);
-   } else {
+  Todo.findByIdAndRemove(req.params.id, function(err, todo){
+    if(err){
+      console.log(err);
+    } else {
       res.json(todo);
-   }
- }); 
+    }
+  });
 });
 
 
